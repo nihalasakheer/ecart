6466571e541501376fad1a1ec137b2953784af86
nihala <nihalazakker1586@gmail.com>
2021-01-25 16:31:32 +0530

    forloop added to shop  page

--- ecart/models.py
+++ ecart/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Materials(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField
+    price = models.IntegerField(default=0)
+    
--- ecart/views.py
+++ ecart/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render, HttpResponse
+from .models import Materials
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def shop(request):
-    return render(request, 'shop.html')
+    mat1 = Materials()
+    mat1.name = 'black kid wear'
+    mat1.img = 'kid1.jpg'
+    mat1.price = 499
+
+    mat2 = Materials()
+    mat2.name = 'black kid wear'
+    mat2.img = 'kid1.jpg'
+    mat2.price = 499
+
+    mat3 = Materials()
+    mat3.name = 'black kid wear'
+    mat3.img = 'kid1.jpg'
+    mat3.price = 499
+
+    mat4 = Materials()
+    mat4.name = 'black kid wear'
+    mat4.img = 'kid1.jpg'
+    mat4.price = 499
+  
+    mats=[mat1,mat2,mat3,mat4]
+
+    return render(request, 'shop.html', {'mats':mats})
 
  
